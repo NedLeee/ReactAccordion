15ba28992452fd318379eba82a93492ef7bf5f66
Courtney Gibson <courtneygibson@Courtneys-MacBook-Air.local>
2024-02-16 17:24:11 -0600

    Youtube FCC 16:00

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,5 +1,26 @@
+//single selection
+//multiple selection
 
+import { useState } from "react";
+import data from "./data.js";
 
 export default function Accordion() {
-    return <div>Accordion</div>;
+
+    const [selected, setSelected] = useState(null);
+
+    return <div className="wrapper">
+        <div className="accordion">
+            {
+                data && data.length > 0 ? 
+                data.map(dataItem => <div className="item">
+                    <div className="title">
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div>
+                </div>) 
+                : <div>No data found!</div>
+            }
+        </div>
+        
+        </div>;
 }
