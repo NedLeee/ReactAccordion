286aba14eca95a37c28d1473f7dd7ef9fb50e0bc
Courtney Gibson <courtneygibson@Courtneys-MacBook-Air.local>
2024-02-21 10:18:55 -0600

    working on star-rating component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
       {/* Accordion component */}
-      {/* <Accordion/> */}
-
+      <Accordion/>
       {/* Random color component */}
       <RandomColor/>
-
+      {/* Star rating component */}
+      <StarRating noOfStars={10} />
     </div>
   );
 }
