8eb96e2c5436538ef6c538682d820a074c752b87
Courtney Gibson <courtneygibson@Courtneys-MacBook-Air.local>
2024-02-23 12:20:18 -0600

    image-slider complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Accordion from './components/accordion';
+import ImageSlider from './components/image-slider';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -7,11 +8,20 @@ function App() {
   return (
     <div className="App">
       {/* Accordion component */}
-      {/* <Accordion/> */}
+      <Accordion/>
+
       {/* Random color component */}
-      {/* <RandomColor/> */}
+      <RandomColor/>
+
       {/* Star rating component */}
       <StarRating noOfStars={10} />
+
+      {/* Image Slider Component */}
+      <ImageSlider 
+        url={'https://picsum.photos/v2/list'} 
+        page={"1"} 
+        limit={"10"}
+        />
     </div>
   );
 }
